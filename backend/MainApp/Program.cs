c4152428709d6752c8ae7d8d1c3d3e0b6a71e501
Buk7op <victor_82hp@mail.ru>
2023-02-16 18:06:34 +0300

    add identity proj

--- backend/MainApp/Program.cs
+++ backend/MainApp/Program.cs
@@ -1,13 +1,27 @@
+using Identity.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var services = builder.Services;
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+services.AddControllers();
+services.AddEndpointsApiExplorer();
+services.AddSwaggerGen();
+
+var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbConfig)).Get<MongoDbConfig>();
+
+services.AddIdentity<ApplicationUser, ApplicationRole>()
+        .AddMongoDbStores<ApplicationUser, ApplicationRole, Guid>
+        (
+            mongoDbSettings.ConnectionString, mongoDbSettings.Name
+        );
+
 
 var app = builder.Build();
+
+
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
